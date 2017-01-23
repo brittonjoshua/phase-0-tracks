acd7cd380b1d2333e2781e60f176b0c498bb6917
Joshua Britton <joshuabritton@Joshuas-MacBook-Pro.local>
2017-01-23 16:03:43 -0700

    updated wordgame file

--- ruby/wordgame/wordgame.rb
+++ ruby/wordgame/wordgame.rb
@@ -1,2 +1,49 @@
-class Wordgame
+#define a class that creates a word guessing game. Instance variables 
+#of word, word length, guessed letters and correct letters.
+class WordGame
+  attr_reader :word, :word_length, :guessed_letters, :correct_letters
+
+  @guess_counter = 0
+
+#Initalize method to set # of guesses as the same # of letters in the #word. A guessed letter array, and the original word split into an #array.
+  
+  def initialize(word)
+    @word = word
+    @word_length = @word.length
+    @correct_letters = @word.split(//)
+    @guessed_letters = [""]
+    @guessed_letters = @guessed_letters.cycle.take @word.length
+  end
+  
+  #A method that iterates through word and if the user selects a correct #letter, then correct letter returns to guessed letters array.
+  
+  def letter_match(letter)
+    @correct_letters.each_with_index do |correct_letter, index|
+      if @correct_letter == letter
+        @guessed_letters << letter
+      end
+    end
+end 
+
+#A winner method that sees if correct letters is the same as guessed letters and returns a message.
+  
+  def winner
+    if @correct_letters.join == @guessed_letters.join
+      p "that's great!"
+    elsif @guess_counter > 5
+      p "sorry you lost, but it'll be ok."
+    else
+      "try again!"
+    end
+  end
 end
+
+# Driver code that sees if any of this works, and it doesnt.
+
+puts "User 1, type in a word:"
+input = gets.chomp
+game = Game.new("#{input}")
+puts "User 2, your word is #{input.length} letters long and you have #{input.length} guesses"
+puts "guess a letter:"
+letter_input = gets.chomp
+game.letter_match(letter_input)
--- ruby/wordgame/wordgame_spec.rb
+++ ruby/wordgame/wordgame_spec.rb
@@ -1,3 +1,20 @@
-retrieve
-class
+require_relative 'wordgame'
+
+describe WordGame do
+	let(:game) { WordGame.new(["hippo"]) }
 end
+
+it "sets the new word upon initialization" do
+	expect(game.initialize).to eq ""
+	end
+
+it "iterates through word and pushes matches to array" do
+	 if @correct_letter == letter
+	 	expect(game.@correct_letter).to eq [0]
+	 end
+
+it "returns a message based on whether letter is in word or not." do
+	 if @correct_letters.join == @guessed_letters.join
+	 	expect(game.winner).to eq "that's great!"
+	 end
+end 
