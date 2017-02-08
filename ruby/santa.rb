e0e8d0872a2c46ab2ff625beb2fdf1fc0165f663
Joshua Britton <joshuabritton@Joshuas-MacBook-Pro.local>
2017-02-08 13:50:08 -0600

    added driver code to santa file

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,14 +1,55 @@
 ## define a Santa class and include 3 instance methods : speak, eat milk and cookies, and initialize.
-
+##Add the attributes of gender (string),ethnicity(string), reindeer_ranking(array), and age(integer) 
+## which is not passed on in initialization.
 class Santa
 
-	def initialize
+	@age = 0
+	@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	
+	def initialize(gender, ethnicity)
+		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+	end 
+
+## add celebrate birthday method that ages santa by one year.
+	def celebrate_birthday
+		new_age = @age +1
 	end 
 
 	def speak
+		puts "Ho, ho, ho happy holidays!"
 	end
 
-	def eat_milk_and_cookies
+## add method that takes reindeers name as an argument and moves it to the last position.
+	def get_mad_at (reindeer)
+		@reindeer_ranking.index(reindeer).rotate
+	end 
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie} cookie!"
 	end
 
+## add setter method that allows gender to be accessed outside class.
+	def gender=(gender)
+		@gender = gender
+	end
 end
+
+##Add test code to initialize different santa instances and call the methods 
+
+santa1 = Santa.new
+p santa1.eat_milk_and_cookies("sugar") 
+santa2 = Santa.new 
+p santa2.eat_milk_and_cookies("gingerbread")
+p santa2.speak
+
+##Driver code, add some new instances of Santa class.
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
