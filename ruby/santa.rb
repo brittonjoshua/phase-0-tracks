daf88d3216360852ac2c3d8d1ea587a4ad2a8c66
Joshua Britton <joshuabritton@Joshuas-MacBook-Pro.local>
2017-02-08 13:53:37 -0600

    added attr_accessor and attr_reader to Santa class.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,11 +1,17 @@
 ## define a Santa class and include 3 instance methods : speak, eat milk and cookies, and initialize.
-##Add the attributes of gender (string),ethnicity(string), reindeer_ranking(array), and age(integer) 
+## Add attributes of reindeer_ranking(array), and age(integer) 
 ## which is not passed on in initialization.
-class Santa
+## Allow age and ethnicity to be readable from outside class.
+## Allow gender to be readable/writable outside class. 
 
+class Santa
+  attr_accessor :gender
+  attr_reader :age, :ethnicity
 	@age = 0
 	@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-	
+
+##Add the attributes of gender (string),ethnicity(string)
+
 	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
 		@gender = gender
@@ -30,17 +36,25 @@ class Santa
 		puts "That was a good #{cookie} cookie!"
 	end
 
-## add setter method that allows gender to be accessed outside class.
-	def gender=(gender)
-		@gender = gender
-	end
+## add setter method that allows gender to be accessed and writable outside class.
+## def gender=(gender)
+##  @gender = gender
+## end
+## add getter methods for age and ethnicity to be accessed/readable outside class.
+## def age
+##  @age
+## end
+
+##	def ethnicity 
+##		@ethnicity 
+##	end
 end
 
 ##Add test code to initialize different santa instances and call the methods 
 
-santa1 = Santa.new
+santa1 = Santa.new("trans", "eskimo")
 p santa1.eat_milk_and_cookies("sugar") 
-santa2 = Santa.new 
+santa2 = Santa.new("female", "Latino") 
 p santa2.eat_milk_and_cookies("gingerbread")
 p santa2.speak
 
@@ -53,3 +67,22 @@ santas << Santa.new("male", "Japanese")
 santas << Santa.new("female", "prefer not to say")
 santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
 santas << Santa.new("N/A", "N/A")
+
+
+##add driver code that will select random gender/ethnicity from example gender and ethnicity arrays.
+santa_gender = ["agender", "male", "female", "transgender", "gender fluid", "bigender"]
+santa_ethnicity = ["black", "Latino", "Japanese", "white", "Korean", "Samoan"]
+
+santa_array = []
+
+50.times do 
+  @gender = santa_gender.sample
+  @ethnicity = santa_ethnicity.sample
+  @age = rand(140)
+  santa_array << Santa.new(@gender, @ethnicity)
+  
+## print out the results of each newly created santa
+puts "This santa is #{@gender} and #{@ethnicity} and is #{@age} years old."
+puts "________________________________________________"
+end 
+  
