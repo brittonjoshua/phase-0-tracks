02a760d6c37ead89e1d985a60b0f8835dbd36c34
Joshua Britton <joshuabritton@Joshuas-MacBook-Pro.local>
2017-02-12 21:31:29 -0600

    updated Rspec file

--- ruby/wordgame/wordgame_spec.rb
+++ ruby/wordgame/wordgame_spec.rb
@@ -1,20 +1,18 @@
 require_relative 'wordgame'
 
 describe WordGame do
-	let(:game) { WordGame.new(["hippo"]) }
+	let(:new_game) { WordGame.new("hippo") }
 end
 
 it "sets the new word upon initialization" do
-	expect(game.initialize).to eq ""
+	expect(new_game.initialize).to eq "hippo"
 	end
 
-it "iterates through word and pushes matches to array" do
-	 if @correct_letter == letter
-	 	expect(game.@correct_letter).to eq [0]
+it "expects the number of guesses to equal number of letters" do
+	 expect(new_game.total_guesses). to eq (5)
 	 end
 
 it "returns a message based on whether letter is in word or not." do
-	 if @correct_letters.join == @guessed_letters.join
-	 	expect(game.winner).to eq "that's great!"
+	 expect(new_game.show_progress('h')). to eq display_string[0]
 	 end
 end 
