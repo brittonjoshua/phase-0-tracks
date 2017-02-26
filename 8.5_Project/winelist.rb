c503c4dd406bcea4eb9fae32a44934c9c4fb4a3d
Joshua Britton <joshuabritton@Joshuas-MacBook-Pro.local>
2017-02-26 16:19:01 -0600

    added database execute info

--- 8.5_Project/winelist.rb
+++ 8.5_Project/winelist.rb
@@ -7,12 +7,18 @@ require 'sqlite3'
 #initialize database
 db = sqlite3::Database.new("wine.db")
 
-#create table upon initialization. Skip if table already exists.
+#create table command. Skip if table already exists.
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS winelist(
     id INTEGER PRIMARY KEY,
     wine_type VARCHAR(255),
     region VARCHAR(255)
-    rating FLOAT(2)
+    rating INTEGER
   )
 SQL
+
+#create a wine table 
+db.execute(create_table_cmd)
+
+#test adding a bottle of wine
+db.execute(INSERT INTO wineList(wine_type,region,rating) VALUES("Malbec","Sonoma County",96))
