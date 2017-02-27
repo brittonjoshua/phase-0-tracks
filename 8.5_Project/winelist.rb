348dff86d5e274218a0d252065d18f62f1e6d022
Joshua Britton <joshuabritton@Joshuas-MacBook-Pro.local>
2017-02-27 09:26:10 -0600

    add wine type and region methods.

--- 8.5_Project/winelist.rb
+++ 8.5_Project/winelist.rb
@@ -3,22 +3,49 @@
 
 #require gems
 require 'sqlite3'
+require 'faker'
 
 #initialize database
 db = sqlite3::Database.new("wine.db")
 
-#create table command. Skip if table already exists.
-create_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS winelist(
+#define a wine table string.
+wine_type_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS wine_type(
     id INTEGER PRIMARY KEY,
-    wine_type VARCHAR(255),
-    region VARCHAR(255)
-    rating INTEGER
+    wine_name VARCHAR(100),
+    vineyard VARCHAR(150),
+    vintage_year INTEGER,
+    rating INTEGER,
+    region_id INTEGER FOREIGN KEY
+  )
+SQL
+#dfine a wine region string.
+region_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS region(
+    id INTEGER PRIMARY KEY,
+    state VARCHAR(100),
+    country VARCHAR(150)
   )
 SQL
 
 #create a wine table 
-db.execute(create_table_cmd)
+db.execute(wine_type_table_cmd)
 
 #test adding a bottle of wine
-db.execute(INSERT INTO wineList(wine_type,region,rating) VALUES("Malbec","Sonoma County",96))
+db.execute("INSERT INTO wine_type (wine_name,vineyard,vintage_year,rating,region_id) VALUES('Malbec','McMannis',1996,86,4)")
+
+#test adding a region table and first region entry.
+db.execute("INSERT INTO region(state,country) VALUES('California','USA')")
+
+#method for adding wine to the wine type table.
+
+def add_wine(db,wine_name,vineyard,vintage_year,rating,region_id)
+	db.execute("INSERT INTO wine_type (wine_name, vineyard,vintage_year,rating,region_id) 
+	VALUES (?,?,?,?,?)",[wine_name,vineyard,vintage_year,rating,region_id])
+end 
+
+#method for adding region to the region table.
+
+def add_region(db, state,country)
+	db.execute("INSERT INTO region (state, country) VALUES(?,?)", [state,country])
+end 
