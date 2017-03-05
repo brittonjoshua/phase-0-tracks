d018968cd8937fef3293cb78689c7d134b1acb43
Joshua Britton <joshuabritton@Joshuas-MacBook-Pro.local>
2017-03-05 16:43:34 -0600

    added appey.rb homey.erb and new_city.erb files

---
+++ web_dev/sinatra_templating/appey.rb
@@ -0,0 +1,27 @@
+# require gems
+require 'sinatra'
+require 'sqlite3'
+
+set :public_folder, File.dirname(__FILE__) + '/static'
+
+db = SQLite3::Database.new("students.db")
+
+#show students database on homepage
+
+get '/' do
+	@students = db.execute("SELECT * FROM students")
+	erb :homey
+end
+
+get '/city/new' do
+  erb :new_city
+end
+
+# create new students via
+# a form
+post '/students' do
+  db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
+  redirect '/'
+end
+
+# add static resources
